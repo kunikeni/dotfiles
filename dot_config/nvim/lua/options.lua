ee6edc8ec8bb0262453c37c8d03342b49ae43296
kunikeni <ota.kuniya@nifty.co.jp>
2026-08-13 09:40:21 +0000

    vimの設定を統一 & 一部バグ修正

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -8,8 +8,9 @@ vim.wo.number = true
 vim.wo.relativenumber = false
 
 -- タブとインデントの設定
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 
@@ -21,10 +22,10 @@ vim.opt.smartcase = true
 vim.opt.cursorline = true
 
 -- spell check
-vim.opt.spell = true
+vim.opt.spell = false
 
 -- カーソル移動
-vim.opt.whichwrap = 'b,s,<,>,[,]'
+vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
 --マウス操作を有効化
 vim.opt.mouse = 'a'
 
@@ -68,3 +69,83 @@ keymap.set('i', '<C-f>', '<Right>')
 keymap.set('i','jj','<Esc>')
 -- 設定ファイルを開く
 keymap.set('n','<F1>',':edit $MYVIMRC<CR>')
+-- マウスドラッグでvisualモードに入らないようにする
+keymap.set({ 'n', 'v', 'o' }, '<LeftDrag>', '<LeftMouse>')
+keymap.set({ 'i', 'c' }, '<LeftDrag>', '<LeftMouse>')
+
+-- ファイルを上書きする前にバックアップを作ることを無効化
+vim.opt.writebackup = false
+-- vim の矩形選択で文字が無くても右へ進める
+vim.opt.virtualedit = 'block'
+-- 全角文字専用の設定
+vim.opt.ambiwidth = 'double'
+-- Windowsでパスの区切り文字をスラッシュで扱う
+vim.opt.shellslash = true
+-- 対応する括弧やブレースを表示
+vim.opt.showmatch = true
+vim.opt.matchtime = 1
+-- インデント方法の変更
+vim.opt.cinoptions:append(':0')
+-- タブ文字を CTRL-I で表示し、行末に $ で表示する
+vim.opt.list = true
+-- 行末のスペースを可視化
+vim.opt.listchars = { tab = '^ ', trail = '~' }
+-- 検索にマッチした行以外を折りたたむ(フォールドする)機能
+vim.opt.foldenable = false
+-- すべての数を10進数として扱う
+vim.opt.nrformats = ''
+-- コメントの色を水色
+vim.cmd('highlight Comment ctermfg=3')
+
+-- Escの2回押しでハイライト消去
+keymap.set('n', '<Esc><Esc>', ':nohlsearch<CR><Esc>')
+
+-- auto reload vimrc
+vim.api.nvim_create_augroup('source_vimrc', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = 'source_vimrc',
+  pattern = { '*vimrc', 'init.lua', 'options.lua' },
+  callback = function()
+    vim.cmd('source $MYVIMRC')
+    vim.opt.foldmethod = 'marker'
+  end,
+})
+
+-- auto comment off
+vim.api.nvim_create_augroup('auto_comment_off', { clear = true })
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = 'auto_comment_off',
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions:remove('r')
+    vim.opt_local.formatoptions:remove('o')
+  end,
+})
+
+-- HTML/XML閉じタグ自動補完
+vim.api.nvim_create_augroup('my_xml', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  group = 'my_xml',
+  pattern = { 'xml', 'html' },
+  callback = function()
+    vim.keymap.set('i', '</', '</<C-x><C-o>', { buffer = true })
+  end,
+})
+
+-- 編集箇所のカーソルを記憶
+vim.api.nvim_create_augroup('redhat', { clear = true })
+vim.api.nvim_create_autocmd('BufRead', {
+  group = 'redhat',
+  pattern = '*.txt',
+  command = 'setlocal textwidth=78',
+})
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = 'redhat',
+  pattern = '*',
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      pcall(vim.cmd, 'normal! g`"')
+    end
+  end,
+})
